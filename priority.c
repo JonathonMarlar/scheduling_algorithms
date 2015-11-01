a7ffcbb3222c95904b297006f7b7c5fd111cc146
Jonathon Marlar <jmarlar@Jonathons-MacBook-Air.local>
2015-11-01 12:18:41 -0600

    Finished shortest and priority scheduling algorithms

---
+++ priority.c
@@ -0,0 +1,150 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NAME_LENGTH 2
+
+typedef struct _job
+{
+    char name[NAME_LENGTH];
+    int arrival_time;
+    int service_time;
+    int priority_level;
+    int wait_time;
+} JOB;
+
+void quicksort(JOB*, int, int);
+
+int main(void)
+{
+    // Initialization
+    FILE *inf            = fopen("in.txt", "r");
+    JOB  temp;
+    fscanf(inf, "%s %d %d %d",
+        temp.name,
+        &temp.arrival_time,
+        &temp.service_time,
+        &temp.priority_level);
+    fgetc(inf);
+    temp.wait_time = 0;
+    JOB  *job_queue      = malloc(sizeof(JOB));
+    int  queue_size      = 0;
+    int  current_job     = 0;
+    int  clock_time      = 0;
+    bool cpu_free        = true;
+
+    // Main clock cycle loop
+    while (clock_time < 200)
+    {
+        // Check job completion in CPU
+        if (cpu_free == false)
+        {
+            // Decrease service time of current job
+            job_queue[current_job].service_time = job_queue[current_job].service_time - 1;
+            if (job_queue[current_job].service_time == 0)
+            {
+                printf("Job complete!\n \
+                    Job Name: %s\n \
+                    Arrival Time: %d\n \
+                    Wait Time: %d\n \
+                    Clock Time of Completion: %d\n",
+                    job_queue[current_job].name,
+                    job_queue[current_job].arrival_time,
+                    job_queue[current_job].wait_time,
+                    clock_time);
+                current_job = current_job + 1;
+                cpu_free = true;
+            }
+        }
+
+        // Check for arrivals in the waiting queue
+        while (temp.arrival_time == clock_time)
+        {
+            strcpy(job_queue[queue_size].name, temp.name);
+            job_queue[queue_size].arrival_time = temp.arrival_time;
+            job_queue[queue_size].service_time = temp.service_time;
+            job_queue[queue_size].priority_level = temp.priority_level;
+            job_queue[queue_size].wait_time = temp.wait_time;
+            queue_size = queue_size + 1;
+            job_queue = realloc(job_queue, (queue_size + 1) * sizeof(JOB));
+            if (fscanf(inf, "%s %d %d %d",
+                temp.name,
+                &temp.arrival_time,
+                &temp.service_time,
+                &temp.priority_level) != EOF)
+            {
+                temp.wait_time = 0;
+            }
+            else
+                {
+                    // it's EOF, so destroy temp
+                    temp.arrival_time = 0;
+                    temp.service_time = 0;
+                    temp.priority_level = 0;
+                }
+            fgetc(inf);
+            quicksort(job_queue, current_job+1, queue_size-1);
+        }
+
+        // Check if CPU is free
+        if (cpu_free && current_job < queue_size)
+        {
+            cpu_free = false;
+        }
+
+        // Increase wait time for every job in queue
+        for (int i = current_job + 1; i < queue_size; i = i + 1)
+        {
+            job_queue[i].wait_time = job_queue[i].wait_time + 1;
+        }
+
+        /*
+        // debug
+        printf("Current Job: %d, Queue Size: %d, Clock Time: %d\n\n", current_job, queue_size, clock_time);
+        for (int i = 0; i < queue_size; i++)
+        {
+            printf("Job Name: %s,\n Arrival time: %d,\n service time: %d,\n priority: %d,\n wait time: %d\n\n",
+                job_queue[i].name, job_queue[i].arrival_time, job_queue[i].service_time, job_queue[i].priority_level, job_queue[i].wait_time);
+        }
+        */
+        // increase clock cycle
+        clock_time = clock_time + 1;
+    }
+
+    free(job_queue);
+
+    return 0;
+}
+
+void quicksort(JOB* arr, int first, int last)
+{
+    int i = first;
+    int j = last;
+    
+    int pivot_a = (i + j) / 2;
+    JOB pivot = arr[pivot_a];
+    
+    while (i <= j)
+    {
+        while (arr[i].priority_level > pivot.priority_level)
+            i++;
+        while (arr[j].priority_level < pivot.priority_level)
+            j--;
+        if (i <= j)
+        {
+            // swap array values
+            JOB temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            // change indeces
+            i++;
+            j--;
+        }
+    }
+    
+    if (first < j)
+        quicksort(arr, first, j);
+    if (i < last)
+        quicksort(arr, i, last);
+}
